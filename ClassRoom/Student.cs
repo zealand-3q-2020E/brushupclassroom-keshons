8561a5b6148ecf007fdbb72dcb1af57ec0ebe068
Keits Buko <keit0130@edu.easj.dk>
2020-09-06 14:06:06 +0200

    Added Student class

---
+++ ClassRoom/Student.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassRoom
+{
+    public class Student
+    {
+        public Student(string name, int birthMonth, int birthday)
+        {
+            Name = name;
+            BirthMonth = birthMonth;
+            Birthday = birthday;
+        }
+
+        public string Name { get; set; }
+
+        public int BirthMonth { get; set; }
+
+        public int Birthday { get; set; }
+    }
+}
