e2cd602d6028984f4e7639c45708e901349728e9
Keits Buko <keit0130@edu.easj.dk>
2020-09-06 14:09:57 +0200

    Added ClassRoom.cs class

---
+++ ClassRoom/ClassRoom.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassRoom
+{
+    class ClassRoom
+    {
+        public ClassRoom()
+        {
+            
+        }
+
+        public string Classname { get; set; }
+        public List<string> Class { get; set; }
+        public DateTime SemesterStart { get; set; }
+    }
+}
